d74aa653265841a0cb4637ae4df807a624b89245
Serkan Ozaydin <serkan.zaydn@gmail.com>
2017-06-13 20:22:01 +0300

    SplashScreenActivity fonksiyonel olarak tamamlandı arkaplan değiştirilecek DrawerActivity oluşturuldu

--- app/src/main/java/hsmnzaydn/serkanozaydin/net/SplashScreenActivity.java
+++ app/src/main/java/hsmnzaydn/serkanozaydin/net/SplashScreenActivity.java
@@ -1,13 +1,43 @@
 package hsmnzaydn.serkanozaydin.net;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private ImageView Icon;
+    private Animation dondurme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        Icon= (ImageView) findViewById(R.id.icon);
+        dondurme= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.dondurme);
+        Icon.startAnimation(dondurme);
+
+        Thread islem=new Thread(){
+            public void run(){
+                try{
+                    sleep(2300);
+                    Intent intent=new Intent(SplashScreenActivity.this,DrawerActivity.class);
+                    startActivity(intent);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    finish();
+                }
+            }
+        };
+        islem.start();
+
+
+
+
     }
 }
